3f24b39419423814d452ac92f1bfde3206c574d3
Jake Marsh <jakemmarsh@gmail.com>
2015-09-03 14:52:19 -0700

    use config.buildDir variable for output

--- gulp/tasks/copyIcons.js
+++ gulp/tasks/copyIcons.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var gulp   = require('gulp');
+var config = require('../config');
 
 gulp.task('copyIcons', function() {
 
   // Copy icons from root directory to build/
   return gulp.src(['./*.png', './favicon.ico'])
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest(config.buildDir));
 
 });
